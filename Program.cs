15b6d1c76fc5e47049ee6e1108b420f0715398be
gabrcastro <gabrielcastromail@gmail.com>
2025-05-22 21:01:26 -0300

    feat: class and methods implemented

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone iphone = new Iphone("01", "16", "123", 256);
+Smartphone nokia = new Nokia("02", "17", "234", 128);
+
+Console.WriteLine($"Iphone {iphone.Numero} ------ ");
+iphone.InstalarAplicativo("Instagram");
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+Console.WriteLine($"Nokia {nokia.Numero} ------ ");
+nokia.InstalarAplicativo("Telegram");
+nokia.Ligar();
+nokia.ReceberLigacao();
